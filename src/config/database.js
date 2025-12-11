6d6b9698835839af0175e36df1687689b2e35f53
junaid.bhat.work <junaid.bhat@semusi.com>
2025-12-11 17:53:07 +0530

    added databaseconnect

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB Connected");
+  } catch (err) {
+    console.log("DB Error", err);
+  }
+}
+
+module.exports = connectDB;
